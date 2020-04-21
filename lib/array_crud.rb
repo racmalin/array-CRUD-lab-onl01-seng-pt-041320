81b3abe2862ef28c2f6ff16dcd7342726417362a
Kevin C Malin <racmalin@yahoo.com>
2020-04-21 04:04:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  ["I", "am", "really", "learning", "arrays!"]
+  ["I", "am", "really", "learning", "arrays!"].pop("arrays!")
 end
 
 # def remove_element_from_start_of_array(array)
