6273fd3783a41b6806605b2de756a3230ff946db
Kevin C Malin <racmalin@yahoo.com>
2020-04-21 03:34:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   ["earth" , "wind" , "water" , "fire"]#sand
+   ["earth" , "wind" , "water" , "fire"] << "sand"
 end
 
 # def add_element_to_start_of_array(array, element)
