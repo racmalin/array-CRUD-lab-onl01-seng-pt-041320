5ac6605d18d419924fe81cb4ad7bf641cae03242
Kevin C Malin <racmalin@yahoo.com>
2020-04-21 04:10:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,9 +22,9 @@ def remove_element_from_start_of_array(array)
   ["wow", "I", "am", "really", "learning", "arrays!"].shift
 end
 
-# def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(array, index_number)
   
-# end
+end
 
 # def retrieve_first_element_from_array(array)
   
